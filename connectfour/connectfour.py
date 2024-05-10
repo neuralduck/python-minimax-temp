66c7b9c9e073b26d31d45e60821db9048ec7c312
neuralduck <135601034+neuralduck@users.noreply.github.com>
2024-05-10 11:15:30 +0530

    connect4 2 player done

--- connectfour/connectfour.py
+++ connectfour/connectfour.py
@@ -1,152 +1,158 @@
 import os
-player1 = 1
-player2 = -1
-symbol1 = '\N{WHITE CIRCLE}'
-symbol2 = '\N{BLACK CIRCLE}'
 
-board = [[0 for _ in range(7)] for _ in range(6)] # 7 x 6 board
-def move(column, mark):
-	for cell in list(range(6))[::-1]:
-		if board[0][column] != 0:
-			print('Column full')
-			break
-		if board[cell][column] == 0:
-			board[cell][column] = mark
-			break
+class ConnectFour:
+	def __init__(self):
+		self.board = [[0 for _ in range(7)] for _ in range(6)]
+		self.symbol1 = '\N{WHITE CIRCLE}'
+		self.symbol2 = '\N{BLACK CIRCLE}'
+		self.move_count = {i: 0 for i in range(7)}
+	def __str__(self):
+		board_str = ''
+		get_symbol = lambda x: f'{self.symbol1}' if x == 1 else f'{self.symbol2}' if x == -1 else ' '
+		dash = '\N{Horizontal Bar}'
+		board_str += '| '+' | '.join(list(map(str, range(7))))+' |'+'\n'
+		board_str += '-'*29+'\n'
+		for row in self.board:
+			board_str += '| '+' | '.join(list(map(get_symbol, row)))+' |'+'\n'
+			board_str += '-'*29+'\n'
+		return board_str
 
-def available_moves():
-	count = 6*7
-	for i in range(6):
-		for j in range(7):
-			if board[i][j] in (1, -1):
-				count -= 1
-	return count
-def print_board():
-	print(*[0, 1, 2, 3, 4, 5, 6])
-	get_symbol = lambda x: symbol1 if x == 1 else symbol2 if x == -1 else '_'
-	for row in board:
-		row = list(map(get_symbol, row))
-		print(*row)
+	def move(self, column, mark):
+		if self.move_count[column] < 6:
+			for cell in list(range(6))[::-1]:
+				if self.board[cell][column] == 0:
+					self.board[cell][column] = mark
+					self.move_count[column] += 1
+					break
 
-def check(board):
-	for i in range(6):
-		for j in range(7):
-			if board[i][j] in (1, -1):
-				if j+3 <= 6:
-					right = 0
-					for n in range(4):
-						right += board[i][j+n]
-					if right == 4: 
-						print(f'right from {i, j}')
-						return 1
-					if right == -4: 
-						print(f'right from {i, j}')
-						return -1
-				if j-3 >= 0:
-					left = 0
-					for n in range(4):
-						left += board[i][j-n]
-					if left == 4:
-						print(f'left from {i, j}')
-						return 1
-					if left == -4:
-						print(f'left from {i, j}')
-						return -1
-				if i-3 >= 0:
-					up = 0
-					for n in range(4):
-						up += board[i-n][j]
-					if up == 4:
-						print(f'up from {i, j}')
-						return 1
-					if up == -4:
-						print(f'up from {i, j}')
-						return -1
-
-				if i+3 <= 5:
-					down = board[i][j] + board[i+1][j] + board[i+2][j] + board[i+3][j]
-					if down == 4:
-						print(f'down from {i, j}')
-						return 1
-					if down == -4:
-						print(f'down from {i, j}')
-						return -1
-
-				if (i-3 >= 0) and (j-3 >= 0):
-					diag_up_left = 0
-					for n in range(4):
-						diag_up_left += board[i-n][j-n]
-					if diag_up_left == 4:
-						print(f'diag up left from {i, j}')
-						return 1
-					if diag_up_left == -4:
-						print(f'diag up left from {i, j}')
-						return -1
-				if (i-3 >= 0) and (j+3 <= 6):
-					diag_up_right = 0
-					for n in range(4):
-						diag_up_right += board[i-n][j+n]
-					if diag_up_right == 4:
-						print(f'diag up right from {i, j}')
-						return 1
-					if diag_up_right == -4:
-						print(f'diag up right from {i, j}')
-						return -1
-				if (i+3 <=5) and (j-3 >= 0):
-					diag_down_left = 0
-					for n in range(4):
-						diag_down_left += board[i+n][j-n]
-					if diag_down_left == 4:
-						print(f'diag down left from {i, j}')
-						return 1
-					if diag_down_left == -4:
-						print(f'diag down left from {i, j}')
-						return -1
-				if (i+3 <= 5) and (j+3 <= 6):
-					diag_down_right = 0
-					for n in range(4):
-						diag_down_right += board[i+n][j+n]
-					if diag_down_right == 4:
-						print(f'diag down right from {i, j}')
-						return 1
-					if diag_down_right == -4:
-						print(f'diag down right from {i, j}')
-						return -1
-	return 0
-turn = 1
-n_moves = {i: 0 for i in range(7)}
-while True:
-	os.system('clear')
-	print(available_moves())
-	print(n_moves)
-	print_board()
-
-	if turn:
-		choice = int(input(f"{symbol1}'s choice: "))
-		if choice not in range(7):
-			print('pick a choice within range')
-			continue
-	else:
-		choice = int(input(f"{symbol2}'s choice: "))
-		if choice not in range(7):
-			print('pick a choice within range')
-			continue
-	n_moves[choice] += 1
-	if turn:
-		move(choice, player1)
-	else:
-		move(choice, player2)
-	turn = int(not(turn))
-	result = check(board)
-	if result == 1:
-		os.system('clear')
-		print_board()
-		print(f'{symbol1} wins')
-		break
-	elif result == -1:
-		os.system('clear')
-		print_board()
-		print(f'{symbol2} wins')
-		break
-
+	def available_moves(self):
+		#returns a list of column numbers with empty cells
+		return list(map(lambda x: x[0], filter(lambda x: True if x[1] < 6 else False, self.move_count.items())))
 	
+	def num_cells(self):
+		#number of empty cells in the board, for draw and game over checks
+		return 42 - sum(self.move_count.values())
+
+	def check(self):
+		# +1 player 1 | -1 player 2 | 0 draw | 3 game not over
+		if self.num_cells() == 0:
+			#draw
+			return 0
+		for i in range(6):
+			for j in range(7):
+				if abs(self.board[i][j]) == 1:
+					RIGHT = (j+3 <= 6)
+					LEFT = (j-3 >= 0)
+					UP = (i-3 >= 0)
+					DOWN = (i+3 <= 5)
+					if RIGHT:
+						right = 0
+						for n in range(4):
+							right += self.board[i][j+n]
+						if abs(right) == 4:
+							return int(right/4)
+						if UP:
+							right_up = 0
+							for n in range(4):
+								right_up += self.board[i-n][j+n]
+							if abs(right_up) == 4:
+								return int(right_up/4)
+						if DOWN:
+							right_down = 0
+							for n in range(4):
+								right_down += self.board[i+n][j-n]
+							if abs(right_down) == 4:
+								return int(right_down/4)
+					if LEFT:
+						left = 0
+						for n in range(4):
+							left += self.board[i][j-n]
+						if abs(left) == 4:
+							return int(left/4)
+
+						if UP:
+							left_up = 0
+							for n in range(4):
+								left_up += self.board[i-n][j-n]
+							if abs(left_up) == 4:
+								return int(left_up/4)
+
+						if DOWN:
+							left_down = 0
+							for n in range(4):
+								left_down += self.board[i+n][j-n]
+							if abs(left_down) == 4:
+								return int(left_down/4)
+					if UP:
+						up = 0
+						for n in range(4):
+							up += self.board[i-n][j]
+						if abs(up) == 4:
+							print(up)
+							print(i, j)
+							return int(up/4)
+
+					if DOWN:
+						down = 0
+						for n in range(4):
+							down += self.board[i+n][j]
+						if abs(down) == 4:
+							print(down)
+							print(i, j)
+							return int(down/4)
+		return 3
+
+
+'''board = ConnectFour()
+board.move(0, -1)
+board.move(0, -1)
+board.move(0, -1)
+board.move(0, -1)
+board.move(0, -1)
+
+print(board.symbol1)
+print(board.symbol2)
+print(board)
+print(board.move_count)
+print(board.available_moves())
+print(board.num_cells())
+print(board.check())'''
+
+if __name__ == '__main__':
+	def info():
+		os.system('clear||cls')
+		print(board)
+	turn = 1
+	board = ConnectFour()
+	player1 = 1
+	player2 = -1
+	while True:
+		info()
+		if turn:
+			choice = int(input(f"Player 1 choice ({board.symbol1}): "))
+		else:
+			choice = int(input(f"Player 2 choice ({board.symbol2}): "))
+		if board.move_count[choice] == 6:
+			print('column full')
+			continue
+		if choice not in range(7):
+			print('pick a choice within range')
+			continue
+		if turn:
+			board.move(choice, player1)
+		else:
+			board.move(choice, player2)
+		turn = int(not(turn))
+		result = board.check()
+		if result == 1:
+			info()
+			print(f'Player 1 ({board.symbol1}) wins')
+			break
+		elif result == -1:
+			info()
+			print(f'Player 2 ({board.symbol2}) wins')
+			break
+		elif result == 0:
+			info()
+			print('Draw')
+			break
